059444895eb7d177f59575a1e0d77a644b81bc4f
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-09-04 23:18:01 +0530

    d21

---
+++ DAY 2 PP/VowelConsonantCounter.java
@@ -0,0 +1,19 @@
+public class VowelConsonantCounter {
+    public static void countVowelsAndConsonants(String text) {
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = Character.toLowerCase(text.charAt(i));
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+}
